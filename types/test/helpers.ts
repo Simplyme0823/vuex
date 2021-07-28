a56509d2509c4e52ab4a515a4a4c404b3dfac5a4
TheNightmareX <TheNightmareX@foxmail.com>
2021-07-28 16:54:36 +0800

    test(types): format

--- types/test/helpers.ts
+++ types/test/helpers.ts
@@ -5,183 +5,182 @@ import {
   mapGetters,
   mapActions,
   mapMutations,
-  createNamespacedHelpers
+  createNamespacedHelpers,
 } from "../index";
 
-const helpers = createNamespacedHelpers('foo');
+const helpers = createNamespacedHelpers("foo");
 
 createApp({
   computed: {
     ...mapState(["a"]),
-    ...mapState('foo', ["b"]),
+    ...mapState("foo", ["b"]),
     ...mapState({
-      c: "c"
+      c: "c",
     }),
-    ...mapState('foo', {
-      d: "d"
+    ...mapState("foo", {
+      d: "d",
     }),
     ...mapState({
-      e: (state: any, getters: any) => state.a + getters.g
+      e: (state: any, getters: any) => state.a + getters.g,
     }),
-    ...mapState('foo', {
-      f: (state: any, getters: any) => state.a + getters.g
+    ...mapState("foo", {
+      f: (state: any, getters: any) => state.a + getters.g,
     }),
 
     ...mapGetters(["g"]),
-    ...mapGetters('foo', ["h"]),
+    ...mapGetters("foo", ["h"]),
     ...mapGetters({
-      i: "i"
+      i: "i",
     }),
-    ...mapGetters('foo', {
-      j: "j"
+    ...mapGetters("foo", {
+      j: "j",
     }),
 
     ...helpers.mapState(["k"]),
     ...helpers.mapState({
-      l: "l"
+      l: "l",
     }),
     ...helpers.mapState({
       m: (state: any, getters: any) => state.a + getters.g,
       useThis(state: any, getters: any): any {
-        return state.a + getters.g + this.whatever
-      }
+        return state.a + getters.g + this.whatever;
+      },
     }),
 
     ...helpers.mapGetters(["n"]),
     ...helpers.mapGetters({
-      o: "o"
+      o: "o",
     }),
 
-
-    otherComputed () {
+    otherComputed() {
       return "";
-    }
+    },
   },
 
   methods: {
     ...mapActions(["p"]),
     ...mapActions({
-      q: "q"
+      q: "q",
     }),
     ...mapActions({
-      r (dispatch, a: string, b: number, c: boolean) {
-        dispatch('p', { a, b, c })
+      r(dispatch, a: string, b: number, c: boolean) {
+        dispatch("p", { a, b, c });
         dispatch({
-          type: 'p',
+          type: "p",
           a,
           b,
-          c
-        })
-      }
+          c,
+        });
+      },
     }),
-    ...mapActions('foo', ["s"]),
-    ...mapActions('foo', {
-      t: "t"
+    ...mapActions("foo", ["s"]),
+    ...mapActions("foo", {
+      t: "t",
     }),
-    ...mapActions('foo', {
-      u (dispatch, a: string, b: number, c: boolean) {
-        dispatch('p', { a, b, c })
+    ...mapActions("foo", {
+      u(dispatch, a: string, b: number, c: boolean) {
+        dispatch("p", { a, b, c });
         dispatch({
-          type: 'p',
+          type: "p",
           a,
           b,
-          c
-        })
-      }
+          c,
+        });
+      },
     }),
 
     ...mapMutations(["v"]),
     ...mapMutations({
-      w: "w"
+      w: "w",
     }),
     ...mapMutations({
-      x (commit, a: string, b: number, c: boolean) {
-        commit('v', { a, b, c })
+      x(commit, a: string, b: number, c: boolean) {
+        commit("v", { a, b, c });
         commit({
-          type: 'v',
+          type: "v",
           a,
           b,
-          c
-        })
-      }
+          c,
+        });
+      },
     }),
-    ...mapMutations('foo', ["y"]),
-    ...mapMutations('foo', {
-      z: "z"
+    ...mapMutations("foo", ["y"]),
+    ...mapMutations("foo", {
+      z: "z",
     }),
-    ...mapMutations('foo', {
-      aa (commit, a: string, b: number, c: boolean) {
-        commit('v', { a, b, c })
+    ...mapMutations("foo", {
+      aa(commit, a: string, b: number, c: boolean) {
+        commit("v", { a, b, c });
         commit({
-          type: 'v',
+          type: "v",
           a,
           b,
-          c
-        })
-      }
+          c,
+        });
+      },
     }),
 
     ...helpers.mapActions(["ab"]),
     ...helpers.mapActions({
-      ac: "ac"
+      ac: "ac",
     }),
     ...helpers.mapActions({
-      ad (dispatch, value: string) {
-        dispatch('p', value)
-      }
+      ad(dispatch, value: string) {
+        dispatch("p", value);
+      },
     }),
 
     ...helpers.mapMutations(["ae"]),
     ...helpers.mapMutations({
-      af: "af"
+      af: "af",
     }),
     ...helpers.mapMutations({
-      ag (commit, value: string) {
-        commit('v', value)
-      }
+      ag(commit, value: string) {
+        commit("v", value);
+      },
     }),
 
-    otherMethod () {}
+    otherMethod() {},
   },
 
   created() {
     // Computed
-    this.a
-    this.b
-    this.c
-    this.d
-    this.e
-    this.f
-    this.g
-    this.h
-    this.i
-    this.j
-    this.k
-    this.l
-    this.m
-    this.n
-    this.o
-    this.otherComputed
+    this.a;
+    this.b;
+    this.c;
+    this.d;
+    this.e;
+    this.f;
+    this.g;
+    this.h;
+    this.i;
+    this.j;
+    this.k;
+    this.l;
+    this.m;
+    this.n;
+    this.o;
+    this.otherComputed;
 
     // Methods
-    this.p()
-    this.q()
-    this.r('', 0, true)
-    this.s()
-    this.t()
-    this.u('', 0, true)
-    this.v()
-    this.w()
-    this.x('', 0, true)
-    this.y()
-    this.z()
-    this.aa('', 0, true)
-    this.ab()
-    this.ac()
-    this.ad('')
-    this.ae()
-    this.af()
-    this.ag('')
-    this.otherMethod()
-  }
+    this.p();
+    this.q();
+    this.r("", 0, true);
+    this.s();
+    this.t();
+    this.u("", 0, true);
+    this.v();
+    this.w();
+    this.x("", 0, true);
+    this.y();
+    this.z();
+    this.aa("", 0, true);
+    this.ab();
+    this.ac();
+    this.ad("");
+    this.ae();
+    this.af();
+    this.ag("");
+    this.otherMethod();
+  },
 });
